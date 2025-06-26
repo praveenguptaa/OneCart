e3a0b2383474b4e06eaf7cd4172a5ccead65a60a
Praveen Gupta <praveenguptaa8@gmail.com>
2025-06-26 19:53:31 +0530

    Modified config

--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -1,12 +1,12 @@
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY ,
-  authDomain: "loginonecart.firebaseapp.com",
-  projectId: "loginonecart",
-  storageBucket: "loginonecart.firebasestorage.app",
-  messagingSenderId: "242165258894",
-  appId: "1:242165258894:web:0155a2ced93e20073247df"
+  apiKey: "AIzaSyCt0mNNzwhG9sLqW8K9kMLY9a_krR0QLDI",
+  authDomain: "onecart-2cc44.firebaseapp.com",
+  projectId: "onecart-2cc44",
+  storageBucket: "onecart-2cc44.firebasestorage.app",
+  messagingSenderId: "335056904332",
+  appId: "1:335056904332:web:42512d6fb7dc51e1c59ba9"
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
